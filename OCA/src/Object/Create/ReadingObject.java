ddcde3969f033e3e1756ac2336ee2f22009baa03
lechiakh <mohammedlechiakh@gmail.com>
2017-12-18 12:16:54 +0000

    the second commit

---
+++ OCA/src/Object/Create/ReadingObject.java
@@ -0,0 +1,15 @@
+package Object.Create;
+
+import java.util.*;
+
+
+		   
+ public class ReadingObject {
+
+	 public static void main(String[] args) {
+		 String x = "Hello World";
+		 String y = " Hello World".trim();
+		 System.out.println(x == y); // true
+		  
+	 }	  
+	}
